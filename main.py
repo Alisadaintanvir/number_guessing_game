10ec0864a7b7faeb9fb5646b504c02629f59e67f
alisadaintanvir <alisadaintanvir@gmail.com>
2023-05-04 16:50:45 +0600

    add docstring to the game function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 from art import logo
 
-easy = 10
-hard = 5
+easy_attempts = 10
+hard_attempts = 5
 random_number = random.randint(1, 100)
 
 print(logo)
@@ -11,7 +11,8 @@ print("I am thinking about a number 1 to 100.")
 playing_mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 
-def make_a_guess(level):
+def game(level):
+    """This function takes the game level as a parameter and compare the answer with the guess number"""
     while level != 0:
         print(f"You have {level} attempts remaining to guess the number.")
         level -= 1
@@ -25,14 +26,14 @@ def make_a_guess(level):
         else:
             print("Too Low")
             print("Guess Again")
-        
+
     else:
         print("You've run out of guesses, you lose.")
 
 
 if playing_mode == "easy":
-    make_a_guess(easy)
+    game(easy_attempts)
 elif playing_mode == "hard":
-    make_a_guess(hard)
+    game(hard_attempts)
 else:
-    print("Incorrect. Please type Correctly.")
+    print("Incorrect. Please type again!")
